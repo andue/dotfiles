372fd34c88f66aa7f65ca82ac8778f768ca5c0fd
Andreas Wagner <andreas.wagner@h-d-gmbh.de>
2015-05-07 09:31:44 +0200

    added some cask apps

--- brew.sh
+++ brew.sh
@@ -3,10 +3,9 @@
 # Install command-line tools using Homebrew.
 
 # Ask for the administrator password upfront.
-sudo -v
-
+#sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -19,7 +18,7 @@ brew install archey node siege tree wget
 
 # Install Cask Apps
 brew install caskroom/cask/brew-cask
-brew cask install alfred2 appcleaner boot2docker dash dropbox google-chrome kaleidoscope spotify sublime-text tower virtualbox
+brew cask install alfred2 appcleaner boot2docker caffeine cheatsheet dash dropbox evernote firefox google-chrome kaleidoscope moom mplayerx spotify sublime-text the-unarchiver tower virtualbox
 
 # Install Cask Quicklook Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package provisionql
