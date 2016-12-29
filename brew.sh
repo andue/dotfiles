bf6f43542f0db42ee15ae360ebc4454e43cfedae
Andreas Wagner <anduee@me.com>
2016-12-29 14:08:01 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -14,14 +14,17 @@ brew update
 brew upgrade
 
 # Install brew apps
-brew install archey httpie imagemagick nvm siege tree wget wireshark
+brew install archey git go httpie imagemagick mas nvm siege tree wget wireshark
 
 # Install Cask Apps
 brew install caskroom/cask/brew-cask
-brew cask install appcleaner atom caffeine cheatsheet dropbox evernote firefox kaleidoscope moneymoney mplayerx paragon-ntfs spotify shiori the-unarchiver tower virtualbox
+brew cask install appcleaner atom caffeine cheatsheet dropbox firefox iterm2 kaleidoscope moneymoney mplayerx paragon-ntfs robomongo spotify shiori the-unarchiver the-unarchiver tower virtualbox
 
 # Install Cask Quicklook Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package provisionql
+
+# Install App Store Apps, pixelmator 1password pages slack magnet tweetbot xcode
+mas install 407963104 443987910 409201541 803453959 441258766 557168941 497799835
 
 # Remove outdated versions from the cellar.
 brew cleanup
