48ff0791bdc5ef28e81ae5b442f48b2a606998e6
Andreas Wagner <anduee@posteo.de>
2018-11-05 19:33:59 +0100

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # Install command-line tools using npm.
-npm install --global fast-cli how2 trash
+npm install --global how2 trash
